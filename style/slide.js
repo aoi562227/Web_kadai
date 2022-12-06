9a7406422dfbc3c4aab289df77d20243cc2ce526
aoi562227 <aoi562227@gmail.com>
2022-12-06 19:18:55 +0900

    1918 disign of intro

---
+++ style/slide.js
@@ -0,0 +1,34 @@
+var sliderWidth = slideWrapper.clientWidth; // container의 width
+var slideIndex = 0;
+var slider = document.querySelector('.slider');
+
+slider.style.width = sliderWidth * totalSlides + 'px';
+
+
+function showSlides(n) {
+    slideIndex = n;
+    if (slideIndex == -1) {
+        slideIndex = totalSlides - 1;
+    } else if (slideIndex === totalSlides) {
+        slideIndex = 0;
+    }
+    slider.style.left = -(sliderWidth * slideIndex) + 'px';
+}
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+  
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+var nextBtn = document.querySelector('.next');
+var prevBtn = document.querySelector('.prev');
+
+nextBtn.addEventListener('click', function () {
+    plusSlides(1);
+});
+prevBtn.addEventListener('click', function () {
+    plusSlides(-1);
+});
